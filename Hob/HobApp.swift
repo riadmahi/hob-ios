468a7fcfc8995bc9b31ad1d45e735f5fb1ca73fb
Riad Mahi <riad.mahi@remedee.com>
2024-11-06 10:08:29 +0100

    Initial Commit

---
+++ Hob/HobApp.swift
@@ -0,0 +1,17 @@
+//
+//  HobApp.swift
+//  Hob
+//
+//  Created by Riad on 06/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct HobApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ HobTests/HobTests.swift
@@ -0,0 +1,16 @@
+//
+//  HobTests.swift
+//  HobTests
+//
+//  Created by Riad on 06/11/2024.
+//
+
+import Testing
+
+struct HobTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
